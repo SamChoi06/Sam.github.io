248170dbf09ebe47cb724081e178de60bb343c51
SamChoi06 <samchoi06@gmail.com>
2022-06-27 18:43:44 +0800

    27062022 1843

--- week2/mission1/wk2_1_lib.py
+++ week2/mission1/wk2_1_lib.py
@@ -16,13 +16,14 @@ def placeFuel():
   if (canPlace == True):
     count_1 = agent.get_item_count(1)
     count_2 = 0
-      if (agent.get_item_count(1) > 0 and agent.get_item(1) == 'bucket'):
+    if (agent.get_item_count(1) > 0 and agent.get_item(1) == 'bucket'):
           agent.give('lava_bucket', 1, 2)
           agent.place(2, 'down')
           count_2 = count_1 - 1
           if (count_1 == 1):
             agent.give('air', 1, 1)
-      else:
-          player.whisper('I don\'t have fuel bucket to place fuel!')
+    else:
+        player.whisper('I don\'t have fuel bucket to place fuel!')
   else:
       player.whisper('I can\'t place fuel at this position!')
+
