3038c790a734bc26418de4cda8273735f7837e72
SamChoi06 <samchoi06@gmail.com>
2022-06-28 11:57:00 +0800

    20220628 1156

--- week2/mission1/wk2_1_lib.py
+++ week2/mission1/wk2_1_lib.py
@@ -1,6 +1,6 @@
 from minecraft.location import as_loc
 
-def pickUpFuel():
+def pickFuel():
   agent.collect("bucket")
   pass
 
@@ -14,16 +14,11 @@ def placeFuel():
           break
 
   if (canPlace == True):
-    count_1 = agent.get_item_count(1)
-    count_2 = 0
-    if (agent.get_item_count(1) > 0 and agent.get_item(1) == 'bucket'):
+      if (agent.get_item_count(1) > 0 and agent.get_item(1) == 'bucket'):
           agent.give('lava_bucket', 1, 2)
           agent.place(2, 'down')
-          count_2 = count_1 - 1
-          if (count_1 == 1):
-            agent.give('air', 1, 1)
-    else:
-        player.whisper('I don\'t have fuel bucket to place fuel!')
+      else:
+          player.whisper('I don\'t have fuel bucket to place fuel!')
   else:
       player.whisper('I can\'t place fuel at this position!')
 
